5389550dee98a798df60d3a628cecbf7d90b5277
Denis Agustin Guerra <denisagustin.guerra@gmail.com>
2022-05-25 21:57:11 -0300

    import y export

--- ejercicios/06-desestructuracion-funcion.ts
+++ ejercicios/06-desestructuracion-funcion.ts
@@ -6,7 +6,7 @@ console.log('Hola Mundo!');
     ===== Código de TypeScript =====
 */
 
-interface Producto{
+export interface Producto{
     desc: string;
     precio: number;
 }
@@ -21,7 +21,7 @@ const tableta: Producto ={
 }
 
 
-function calculaISV(productos:Producto[]):[number,number]{
+export function calculaISV(productos:Producto[]):[number,number]{
     let total=0;
     productos.forEach( ({ precio}) =>{
         total+=precio;
